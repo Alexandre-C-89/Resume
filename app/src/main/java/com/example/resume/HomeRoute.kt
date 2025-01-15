e00c4c40733d97aa10bdc34634be3d027570b633
Clémençot Alexandre <73541197+Alexandre-C-89@users.noreply.github.com>
2025-01-15 10:27:49 +0100

    Modification HomeRoute

--- app/src/main/java/com/example/resume/HomeRoute.kt
+++ app/src/main/java/com/example/resume/HomeRoute.kt
@@ -1,9 +1,14 @@
 package com.example.resume
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.resume.designsystem.AppScaffold
 import com.example.resume.designsystem.bar.TopBar
 
@@ -22,11 +27,17 @@ fun HomeScreen(
 
 ){
    AppScaffold(
-    topBar = {
-        TopBar(text = "Alexandre")
-    }
+       topBar = {
+            TopBar(text = "Alexandre")
+       }
    ) {
-       Text(text = "Welcome")
+       Column(
+           modifier = Modifier
+               .fillMaxSize()
+               .padding(8.dp)
+       ) {
+           Text(text = "Welcome")
+       }
    }
 }
 
